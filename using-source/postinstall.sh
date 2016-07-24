bead9c0b780570e2a0c3dabbb0aa55d8a3461c7e
Mike Macaulay <mmacaula@gmail.com>
2016-07-24 15:19:12 -0500

    updated to 1.23 and fixed postinstall to not call ant

--- using-source/postinstall.sh
+++ using-source/postinstall.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 rm -rf public && mkdir public
-cd node_modules/cesium
-ant
-cd ../../
 cp -r ./node_modules/cesium/Build/Cesium/* ./public
-find node_modules/cesium/Source -name "package.json" -type f -delete
